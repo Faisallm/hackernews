593d12e9f144cf08c3c3b79753f5c05f5a8e9ddb
Faisallm <faisallawan1997@gmail.com>
2023-08-21 03:20:01 -0700

    gear 2nd

--- src/App.js
+++ src/App.js
@@ -21,17 +21,36 @@ const list = [
   },
 ]
 
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
 
   // gives us access to this.props
+  // the contructor is only there to instantiate...
+  // your class with all its properties
   constructor(props) {
     super(props);
 
     // internal component state
     this.state = {
       list,
+      searchTerm: "",
     }
 
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+
+  }
+
+  onDismiss(id) {
+    // filter out list items with ids that do not match...
+    // the clicked item id.
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({list: updatedList})
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value })
   }
 
   render() {
@@ -40,7 +59,12 @@ class App extends Component {
 
       <div className="App">
 
-        {this.state.list.map(item =>
+        <form>
+          <input type="text" 
+          onChange={this.onSearchChange}/>
+        </form>
+
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -48,6 +72,13 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+
+            <span>
+              <button onClick={() => this.onDismiss(item.objectID)} type='button'>
+                Dismiss
+              </button>
+            </span>
+
           </div>
         )}
 
